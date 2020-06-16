c8b71a82ecca1d44c9344b09208ef844e97a4b02
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2020-06-16 22:33:23 +0200

    Create dummy list in app.js

--- src/main/App.js
+++ src/main/App.js
@@ -3,6 +3,44 @@ import "../styles/App.css";
 import Form from "../components/Form";
 import List from "../components/List";
 
+const list = [
+  {
+    id: 1,
+    task: "Kupic samochód",
+    date: "2020-07-23",
+    isImportant: false,
+    active: false,
+  },
+  {
+    id: 2,
+    task: "Zagarać w wiedźmina",
+    date: "2020-09-03",
+    isImportant: false,
+    active: false,
+  },
+  {
+    id: 3,
+    task: "Jeść więcej",
+    date: "2020-06-17",
+    isImportant: false,
+    active: false,
+  },
+  {
+    id: 4,
+    task: "umyć rower",
+    date: "2020-07-13",
+    isImportant: false,
+    active: false,
+  },
+  {
+    id: 5,
+    task: "uczyć się programowania",
+    date: "2020-06-16",
+    isImportant: false,
+    active: false,
+  },
+];
+
 class App extends Component {
   render() {
     return (
